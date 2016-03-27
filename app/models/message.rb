e271ee23f495b4ec0f81edb10a731c61cb54b14a
ohiroyu333 <o_hiroyu333@yahoo.co.jp>
2016-03-27 11:26:46 +0000

    20160327

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
   # 年齢は必須入力かつ0以上の数字
-  validates :age , numericality: { greater_than: 0 } , presence: true
+  validates :age , numericality: { only_integer: true, greater_than: 0 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } 
 end
